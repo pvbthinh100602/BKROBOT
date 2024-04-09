d6bae5bedaace63d800cd44f085462bc3bd6dd65
pvbt2002 <phamvietbaothinh2002@gmail.com>
2024-04-09 22:03:30 +0700

    fix servo test

--- Core/Src/servo.c
+++ Core/Src/servo.c
@@ -25,7 +25,7 @@ void testServo(){
 	servo_count++;
 	if(servo_count == 500){
 		servo_count = 0;
-//		angle += 90;
+		angle += 90;
 		if(angle > 180) angle = 0;
 		for(int i = 0; i < 3; i++){
 			servo_set_angle(servo_channel[i],angle);

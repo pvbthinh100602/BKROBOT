ddfb2c6c1e6241d44fabbd3c0d38be28a239e530
pvbt2002 <phamvietbaothinh2002@gmail.com>
2024-04-15 10:46:10 +0700

    add display file

---
+++ Core/Inc/display.h
@@ -0,0 +1,13 @@
+/*
+ * display.h
+ *
+ *  Created on: Apr 15, 2024
+ *      Author: phamv
+ */
+
+#ifndef INC_DISPLAY_H_
+#define INC_DISPLAY_H_
+
+#include "spi.h"
+
+#endif /* INC_DISPLAY_H_ */
---
+++ Core/Src/display.c
@@ -0,0 +1,8 @@
+/*
+ * display.c
+ *
+ *  Created on: Apr 15, 2024
+ *      Author: phamv
+ */
+
+#include "display.h"
